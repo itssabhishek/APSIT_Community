214c8ba85340990b639888bb579bf91cfacf6a40
itssabhishek <itsabhishek0710@gmail.com>
2023-03-22 20:52:51 +0530

    Fixed few security issue

--- next.config.js
+++ next.config.js
@@ -11,12 +11,6 @@ const withTM = require('next-transpile-modules')([
 const nextConfig = {
   swcMinify: false,
   trailingSlash: true,
-  env: {
-    HOST_API_KEY: 'https://apsit-community-15wd.onrender.com',
-    AWS_ACCESS_KEY_ID: 'AKIASLOX5QWLI6XWQJCR',
-    AWS_SECRET_ACCESS_KEY: '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',
-    AWS_REGION: 'ap-south-1',
-  },
 };
 
 const pwaConfig = {
